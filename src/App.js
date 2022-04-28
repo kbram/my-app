0e38d7a3044a8dd6cfa46dc6756d870e08e2ec99
kbram <nkbram95@gmail.com>
2022-04-28 16:32:23 +0530

    test husky real

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          testing git
+          testing git husky
         </a>
       </header>
     </div>
